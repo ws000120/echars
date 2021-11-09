cf790032846cf51b2eac39b06c80fe6ed3311098
ws000120 <211380370@qq.com>
2021-11-09 13:58:17 +0800

    柱状图案例

---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,130 @@
+<template>
+  <el-container id="layout-container">
+    <el-header class="layout-header">
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light"></div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main class="layout-content">
+      <el-row type="flex" class="row-bg">
+        <el-col :span="6">
+          <div class="grid-content">
+            <bar></bar>
+          </div>
+          <div class="grid-content">
+
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content"></div>
+          <div class="grid-content"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content"></div>
+          <div class="grid-content"></div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content"></div>
+          <div class="grid-content"></div>
+        </el-col>
+      </el-row>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import Bar from '@/components/bar'
+
+  export default {
+    name: "Layout",
+    components: {Bar},
+    data() {
+      return {}
+    },
+    methods: {
+      bg_color() {
+        let el = $('.grid-content')
+        for (let i = 0; i < el.length; i++) {
+          let r = Math.floor(Math.random() * 256);
+          let g = Math.floor(Math.random() * 256);
+          let b = Math.floor(Math.random() * 256);
+          let c = `rgb(${r},${g},${b})`
+          $(el[i]).css('background-color', c)
+        }
+      }
+    },
+    mounted() {
+      this.bg_color()
+    },
+  }
+</script>
+
+<style lang='less' scoped>
+  #layout-container {
+    height: inherit;
+
+    /deep/ .el-header {
+      .el-row {
+        height: 50%;
+        width: 100%;
+
+        .el-col {
+          height: 100%;
+        }
+      }
+
+      .grid-content {
+        overflow: auto;
+        height: 100%;
+      }
+    }
+
+    .layout-content {
+      height: 100%;
+
+      .row-bg {
+        height: 100%;
+        overflow: auto;
+
+        .grid-content {
+          height: 50%;
+        }
+
+
+      }
+    }
+
+
+  }
+</style>
---
+++ src/components/bar/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :id="id">
+
+  </div>
+</template>
+
+<script>
+  import * as echarts from 'echarts';
+  import {resize} from "@/util/tool";
+
+  export default {
+    name: "index",
+    data() {
+      return {
+        id: 'bar_canvas',
+        myChart: null,
+        option: {},
+        color: {}
+      }
+    },
+    methods: {
+      myChart_render() {
+        // 基于准备好的dom，初始化echarts实例
+        $("#bar_canvas").height($("#bar_canvas").parent().height())
+        this.myChart = echarts.init(document.getElementById(this.id));
+        // 绘制图表
+        this.option = {
+          title: {
+            text: 'ECharts 柱状图示例'
+          },
+          tooltip: {},
+          xAxis: {
+            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          },
+          yAxis: {},
+          series: [
+            {
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+            }
+          ]
+        }
+        this.myChart && this.myChart.setOption(this.option);
+        resize(this.myChart, $("#bar_canvas"), $("#bar_canvas").parent())
+      }
+    },
+    mounted() {
+      this.myChart_render()
+    },
+  }
+</script>
+
+<style lang='' scoped>
+
+</style>
