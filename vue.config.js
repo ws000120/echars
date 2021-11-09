5b829626a053a5dc8fa55a22a61501ed4b930b74
ws000120 <211380370@qq.com>
2021-11-09 08:42:11 +0800

    修改

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack')
+module.exports = {
+  chainWebpack: config => {
+    config.plugin('provide').use(webpack.ProvidePlugin, [{
+      $: 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }])
+  }
+}
